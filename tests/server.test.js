30808d840c24377549463a0f01395885df37bda0
Jordan Bohmbach <jbohmbach@simplefractal.com>
2023-10-19 20:49:22 -0500

    implemented tests for the desired server.js functionality

--- tests/server.test.js
+++ tests/server.test.js
@@ -12,6 +12,61 @@ describe('City Population API', () => {
       .end((err, res) => {
         expect(res).to.have.status(200);
         expect(res.body.population).to.be.a('number');
+        expect(res.body.population).to.equal(32423);
+        done();
+      });
+  });
+
+  it('should return an error for a non-existent city and state combo', done => {
+    chai.request(server)
+      .get('/api/population/state/Nowhere/city/NoCity')
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.error).to.equal('State/city combo not found.');
+        done();
+      });
+  });
+
+  it('should update population for an existing city and state', done => {
+    const newPopulation = 123456;
+    chai.request(server)
+      .put('/api/population/state/Florida/city/Orlando')
+      .send({ population: newPopulation })
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+
+  it('should create a new city and state with given population', done => {
+    const newPopulation = 789012;
+    chai.request(server)
+      .put('/api/population/state/NewState/city/NewCity')
+      .send({ population: newPopulation })
+      .end((err, res) => {
+        expect(res).to.have.status(201);
+        expect(res.body.message).to.equal('Created successfully.');
+        done();
+      });
+  });
+
+  it('should return an error for invalid population data', done => {
+    chai.request(server)
+      .put('/api/population/state/Florida/city/Orlando')
+      .send({ population: "invalid" })
+      .end((err, res) => {
+        expect(res).to.have.status(400);
+        expect(res.body.error).to.equal('Invalid population data.');
+        done();
+      });
+  });
+
+  it('should return an error for an invalid route', done => {
+    chai.request(server)
+      .get('/api/invalid/route')
+      .end((err, res) => {
+        expect(res).to.have.status(404);
+        expect(res.body.error).to.equal('Invalid route. Please use /api/population/state/:state/city/:city');
         done();
       });
   });
